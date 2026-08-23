643409cfbc58f53d80a2692ed56fb9e33d6a6f2b
nholms <nholms@users.noreply.github.com>
2026-08-23 19:55:23 -0400

    Update models.rs

--- 4. Projects/1. CLI/Problem/Stage 1/Step  1/src/models.rs
+++ 4. Projects/1. CLI/Problem/Stage 1/Step  1/src/models.rs
@@ -1,28 +1,50 @@
 pub enum Status {
-    // TODO: add fields (make sure the fields are public)
+    Open,
+    InProgress,
+    Resolved,
+    Closed,
 }
 
 pub struct Epic {
-    // TODO: add fields (make sure the fields are public)
+    pub id: i32,
+    pub name: String,
+    pub description: String,
+    pub status: Status,
+    pub stories: Vec<Story>,
 }
 
 impl Epic {
     pub fn new(name: String, description: String) -> Self {
-        todo!() // by default the status should be set to open and the stories should be an empty vector
+        Epic {
+            id: 0,
+            name: name,
+            description: description,
+            status: Status::Open,
+            stories: vec![],
+        }
     }
 }
 
 pub struct Story {
-    // TODO: add fields (make sure the fields are public)
+    pub id: i32,
+    pub name: String,
+    pub description: String,
+    pub status: Status,
 }
 
 impl Story {
     pub fn new(name: String, description: String) -> Self {
-        todo!() // by default the status should be set to open
+        Story {
+            id: 0,
+            name: name,
+            description: description,
+            status: Status::Open,
+        }
     }
 }
 
 pub struct DBState {
-    // This struct represents the entire db state which includes the last_item_id, epics, and stories
-    // TODO: add fields (make sure the fields are public)
+    pub last_item_id: i32,
+    pub epics: Vec<Epic>,
+    pub stories: Vec<Story>,
 }
